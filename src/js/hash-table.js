f690f98ef664a7e4f3670c36c427bae1757f02cc
Marknjo <marknjo7@gmail.com>
2023-03-01 09:26:11 +0300

    feat(HashTable): implement get method

--- src/js/hash-table.js
+++ src/js/hash-table.js
@@ -26,14 +26,42 @@ class HashTable {
     console.timeEnd("Set");
   }
 
+  get(key) {
+    let address = this.#hash(key);
+
+    if (!this.#data[address]) {
+      return undefined;
+    }
+
+    /// Don't repeat asking for address value(s)
+    let foundAddress = this.#data[address];
+
+    /// If no-collision, and there is only one value in the same options
+    if (foundAddress.length === 1) {
+      return foundAddress[0][1];
+    }
+
+    /// Multiple values in the same option
+    let foundValues = foundAddress.map((value) => {
+      if (value[0] === key) {
+        return value[1];
+      }
+    });
+
+    return foundValues.length > 1 ? foundValues : foundValues[0];
+  }
+
   printData() {
     return this.#data;
   }
 }
 
-const hashTable = new HashTable(2);
+const hashTable = new HashTable(50);
 
 hashTable.set("grapes", 10000);
-hashTable.set("grapes", 10000);
+hashTable.set("grapes", 50);
+hashTable.set("pineapples", 104);
+
+console.log(hashTable.get("pineapples"));
 
 console.log(hashTable.printData());
