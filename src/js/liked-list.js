cb78bf030bd44158777214a7a8587ee99a86aec4
Marknjo <marknjo7@gmail.com>
2023-03-02 10:02:06 +0300

    feat(LinkedList): add append method

--- src/js/liked-list.js
+++ src/js/liked-list.js
@@ -10,8 +10,37 @@ class List {
     this.tail = this.head;
     this.length = value ? 1 : 0;
   }
+
+  append(value) {
+    // Handle list was initialize without default values
+    if (this.length === 0) {
+      this.head = {
+        value,
+        next: null,
+      };
+
+      this.tail = this.head;
+    }
+
+    /// List was initializes with default values
+    if (this.length !== 0) {
+      const newNode = {
+        value,
+        next: null,
+      };
+
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
-const list = new List();
+const list = new List(12);
+
+list.append(10);
+list.append(2);
 
 console.log(list);
