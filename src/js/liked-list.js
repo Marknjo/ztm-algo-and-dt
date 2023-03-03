00a7e3c68debb71fbe49dc76a70595d7ce7bca9c
Marknjo <marknjo7@gmail.com>
2023-03-03 17:01:59 +0300

    feat(LinkedList): add prepend method

--- src/js/liked-list.js
+++ src/js/liked-list.js
@@ -19,12 +19,30 @@ class List {
     this.#initList(value, newNode);
   }
 
+  prepend(value) {
+    const newNode = new Node(value);
+
+    // handle empty list
+    if (this.length === 0) {
+      this.#initList(value, newNode);
+      return this.values();
+    }
+
+    // handle list with head
+    const currentHeadNode = this.#head;
+    this.#head = newNode.head;
+    this.#head.next = currentHeadNode;
+    this.length++;
+
+    return this.values();
+  }
+
   append(value) {
     const newNode = new Node(value);
     // Handle list was initialize without default values
     if (this.length === 0) {
       this.#initList(value, newNode);
-      return this;
+      return this.values();
     }
 
     /// List was initializes with default values
@@ -32,7 +50,7 @@ class List {
     this.#tail = newNode.head;
 
     this.length++;
-    return this;
+    return this.values();
   }
 
   values() {
@@ -64,5 +82,8 @@ const list = new List(12);
 list.append(10);
 list.append(2);
 list.append(4);
+list.prepend(5);
+list.prepend(-1);
 
-console.log(list.values());
+console.log(list.values(), list.length);
+console.log(list.showList());
