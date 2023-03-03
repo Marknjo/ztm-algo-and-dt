7c78105327e589fa3fb7d642dd5e4b9c058f4906
Marknjo <marknjo7@gmail.com>
2023-03-03 16:42:15 +0300

    refactor(LinkedList): add list initializer reusable helper method

--- src/js/liked-list.js
+++ src/js/liked-list.js
@@ -16,20 +16,14 @@ class List {
 
   constructor(value) {
     const newNode = new Node(value);
-    this.#head = newNode.head;
-
-    this.#tail = this.#head;
-    this.length = value ? 1 : 0;
+    this.#initList(value, newNode);
   }
 
   append(value) {
     const newNode = new Node(value);
     // Handle list was initialize without default values
     if (this.length === 0) {
-      this.#head = newNode.head;
-      this.#tail = newNode.head;
-
-      this.length++;
+      this.#initList(value, newNode);
       return this;
     }
 
@@ -56,11 +50,19 @@ class List {
 
     return listItem;
   }
+
+  #initList(value, newNode) {
+    this.#head = newNode.head;
+
+    this.#tail = this.#head;
+    this.length = value ? 1 : 0;
+  }
 }
 
 const list = new List(12);
 
 list.append(10);
 list.append(2);
+list.append(4);
 
 console.log(list.values());
