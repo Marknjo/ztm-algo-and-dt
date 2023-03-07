57ff3c77cd9e17ca6f1b68cd81db86a6d7cab170
Marknjo <marknjo7@gmail.com>
2023-03-07 08:35:03 +0300

    feat(Queue): implement enqueue method

--- src/js/queue.js
+++ src/js/queue.js
@@ -4,7 +4,7 @@ class Node {
 
   constructor(value) {
     this.value = value;
-    this.next = value;
+    this.next = null;
   }
 }
 
@@ -17,9 +17,43 @@ class Queue {
     return this.#top;
   }
 
-  enqueue(value) {}
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.#top = newNode;
+      this.#bottom = newNode;
+
+      this.length++;
+      return this;
+    }
+
+    let currentBottom = this.#bottom;
+    this.#bottom = newNode;
+    this.#bottom.next = currentBottom;
+    this.length++;
+
+    return this;
+  }
 
   dequeue() {}
 }
 
 const queue = new Queue();
+
+//["Charlie", "Ella", "Rachel", "Ethan", "Avery", "Max", "Olivia", "Kaia", "Samantha", "Oscar"]
+
+queue.enqueue("Charlie");
+queue.enqueue("Ella");
+queue.enqueue("Rachel");
+queue.enqueue("Ethan");
+queue.enqueue("Avery");
+queue.enqueue("Max");
+queue.enqueue("Olivia");
+queue.enqueue("Kaia");
+queue.enqueue("Samantha");
+queue.enqueue("Oscar");
+
+console.log(queue.peek());
+
+console.log(queue);
