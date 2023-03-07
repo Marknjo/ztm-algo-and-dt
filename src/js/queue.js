01921f155b2365288f0a6cb9a81aef66446cfaf8
Marknjo <marknjo7@gmail.com>
2023-03-07 08:49:00 +0300

    feat(Queue): implement dequeue method

--- src/js/queue.js
+++ src/js/queue.js
@@ -5,12 +5,16 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
+/**
+ * Implemented Queues using Doubly links
+ */
 class Queue {
-  #top = 0;
-  #bottom = 0;
+  #top = null;
+  #bottom = null;
   length = 0;
 
   peek() {
@@ -31,18 +35,30 @@ class Queue {
     let currentBottom = this.#bottom;
     this.#bottom = newNode;
     this.#bottom.next = currentBottom;
+    this.#bottom.next.prev = this.#bottom;
+
     this.length++;
 
     return this;
   }
 
-  dequeue() {}
+  dequeue() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    this.#top = this.#top.prev;
+    this.#top.next = null;
+
+    this.length--;
+  }
 }
 
 const queue = new Queue();
 
 //["Charlie", "Ella", "Rachel", "Ethan", "Avery", "Max", "Olivia", "Kaia", "Samantha", "Oscar"]
 
+/// Enqueue tests
 queue.enqueue("Charlie");
 queue.enqueue("Ella");
 queue.enqueue("Rachel");
@@ -55,5 +71,18 @@ queue.enqueue("Samantha");
 queue.enqueue("Oscar");
 
 console.log(queue.peek());
+console.log(queue);
+
+/// Dequeue tests
+console.log("\n----------------------DEQUEUE----------------------\n");
+
+queue.dequeue();
+console.log(queue.peek()); // Expects Ella
+
+queue.dequeue();
+console.log(queue.peek()); // Expects Rachel
+
+queue.dequeue();
+console.log(queue.peek()); // Expects Ethan
 
 console.log(queue);
