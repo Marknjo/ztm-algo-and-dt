5a026203d9360587d4e144ab2ca9a9151aa6b045
Marknjo <marknjo7@gmail.com>
2023-03-28 09:26:42 +0300

    algo(BST): implement find/contains method

--- src/js/binary-BST.js
+++ src/js/binary-BST.js
@@ -47,6 +47,70 @@ class BST {
 
     return this;
   }
+
+  find(val) {
+    if (val == "" || typeof val === "undefined") return undefined;
+
+    if (!this.#root) return null;
+
+    if (!this.#root.left && !this.#root.right) {
+      return this.#root.value === val ? this.#root : null;
+    }
+
+    // more than one value in the tree
+
+    let current = this.#root;
+    let found = null;
+
+    while (current && !found) {
+      let currentValue = current.value;
+      if (val === currentValue) found = current;
+
+      // find left
+      if (val < currentValue) {
+        current = current.left;
+      }
+
+      // find right
+      if (val > currentValue) {
+        current = current.right;
+      }
+    }
+
+    return found;
+  }
+
+  contains(val) {
+    if (val == "" || typeof val === "undefined") return undefined;
+
+    if (!this.#root) return false;
+
+    if (!this.#root.left && !this.#root.right) {
+      return this.#root.value === val ? true : false;
+    }
+
+    // more than one value in the tree
+
+    let current = this.#root;
+
+    while (true) {
+      let currentValue = !!current?.value ? current.value : false;
+
+      if (!currentValue) return false;
+
+      if (val === currentValue) return true;
+
+      // find left
+      if (val < currentValue) {
+        current = current.left;
+      }
+
+      // find right
+      if (val > currentValue) {
+        current = current.right;
+      }
+    }
+  }
 }
 
 let tree = new BST();
@@ -56,9 +120,16 @@ console.log("--------------INSERT METHOD--------------");
 //   5      13
 // 2   7 11  16
 console.log(tree.insert(10));
-console.log(tree.insert(5));
-console.log(tree.insert(7));
-console.log(tree.insert(13));
-console.log(tree.insert(2));
-console.log(tree.insert(11));
-console.log(tree.insert(16));
+// console.log(tree.insert(5));
+// console.log(tree.insert(7));
+// console.log(tree.insert(13));
+// console.log(tree.insert(2));
+// console.log(tree.insert(11));
+// console.log(tree.insert(16));
+
+console.log("--------------SEARCH METHOD--------------");
+
+console.log(tree.contains(7));
+console.log(tree.find(5));
+console.log(tree.find(15));
+console.log(tree.find(10));
